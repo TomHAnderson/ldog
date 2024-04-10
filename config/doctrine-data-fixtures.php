547d85ecd9e54f5e626a18e2824ea85845fff494
Tom H Anderson <tom.h.anderson@gmail.com>
2024-04-10 02:00:29 -0600

    phpcs doctrine-data-fixtures

--- config/doctrine-data-fixtures.php
+++ config/doctrine-data-fixtures.php
@@ -1,5 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+
 /**
  * In the default values listed below, ORM fixtures are configured.  You may
  * configure other Doctrine fixture group types:
@@ -8,19 +13,19 @@
  * ORMPurger   | PHPCRPurger   | MongoDBPurger
  */
 
+// The order of fixtures in the fixtures array is not the order in which
+// they will be executed.  See
+// https://github.com/doctrine/data-fixtures#fixture-ordering
+/**
+Fixture1::class,
+Fixture2::class,
+*/
+
 return [
     'default' => [  // Group name
         'objectManager' => 'Doctrine\ORM\EntityManager',
-        'executor' => \Doctrine\Common\DataFixtures\Executor\ORMExecutor::class,
-        'purger' => \Doctrine\Common\DataFixtures\Purger\ORMPurger::class,
-        'fixtures' => [
-            // The order of fixtures in this list is not the order in which
-            // they will be executed.  See
-            // https://github.com/doctrine/data-fixtures#fixture-ordering
-            /**
-             Fixture1::class,
-             Fixture2::class,
-             */
-        ],
+        'executor' => ORMExecutor::class,
+        'purger' => ORMPurger::class,
+        'fixtures' => [],
     ],
 ];
