7d9d40581ed923012d9698b721bd03314cc6bc02
Tom H Anderson <tom.h.anderson@gmail.com>
2024-05-02 23:54:55 -0600

    Dynamic logo width

---
+++ public/docs/_app/immutable/nodes/2.B6rwUsBJ.js
@@ -0,0 +1,1 @@
+import{r as e}from"../chunks/index.C0S8Ougm.js";import{h as r}from"../chunks/pages.B8Cw7v6f.js";const o=!0;function t(){throw e(301,r)}const l=Object.freeze(Object.defineProperty({__proto__:null,load:t,prerender:o},Symbol.toStringTag,{value:"Module"}));export{l as universal};
