f89c6429b7359b15066cd3471e3df97503556221
Tom H Anderson <tom.h.anderson@gmail.com>
2024-05-28 18:34:30 -0600

    Help Resources for standards

---
+++ public/docs/_app/immutable/nodes/2.jA93F0WW.js
@@ -0,0 +1,1 @@
+import{r as e}from"../chunks/index.G34q7yGZ.js";import{h as r}from"../chunks/pages.DA7UdULK.js";const o=!0;function t(){throw e(301,r)}const l=Object.freeze(Object.defineProperty({__proto__:null,load:t,prerender:o},Symbol.toStringTag,{value:"Module"}));export{l as universal};
